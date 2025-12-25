d027cc93f53d519b1e75f4d1151ea5c2b56f0d21
bongjoonno <zachary.feryate@gmail.com>
2025-12-25 07:06:15 -0800

    added to-do's for tomorrow

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    
+'''
+TO-DO
+
+1. Remove Randomness and Set Seeds
+2. Optimize Catboost LR and dpeth
+3. Add XGBoost Model
+4. Add TABPFN Model
+5. Do Feature Engineering
+6. 
+
+
+'''
