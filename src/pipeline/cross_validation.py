6bb6b2eee10af54a4e148051b324ef0610aea43a
bongjoonno <zachary.feryate@gmail.com>
2025-12-25 06:11:25 -0800

    finished crossval pipelien

--- src/pipeline/cross_validation.py
+++ src/pipeline/cross_validation.py
@@ -1,43 +1,53 @@
-def train_cross_val(model, clean_data_args: dict, scale_x: bool, optimize_thresh: bool):
-  x, y = make_x_and_y(df, clean_data_args)
+from imports import np, f1_score
+from src.data_prep import clean_data, train_df, split_data_k_folds, scale_data
+from src.prediction_creation import make_preds_with_thresholds
+from src.optimizers import ThresholdOptimizer
 
-  f1s_per_class = []
-  avg_f1s = []
+threshold_optimizer = ThresholdOptimizer()
 
-  n_folds = 5
-  folds = split_k_folds(x, y, n_folds)
+class CrossValPipeline:
+    n_folds = 5
+    
+    @staticmethod
+    def train_cross_val(model_package: dict):
+        model = model_package['model']
+        one_hot_encode_categoricals = model_package['one_hot_encode_categoricals']
+        scale_x = model_package['scale_x']
+        optimize_thresh = model_package['optimize_thresh']
+            
+        df = clean_data(train_df, one_hot_encode_categoricals)
+            
+        x = df.drop(columns='Target')
+        y = df['Target']
+            
+        folds = split_data_k_folds(x, y, CrossValPipeline.n_folds)
+            
+        f1s_per_class = []
+        avg_f1s = []
 
-  avg_threshes = [[], [], []]
-  for fold in folds:
-    x_train, y_train, x_val, y_val = fold
+        avg_threshes = [[], [], []]
+        
+        for fold in folds:
+            x_train, y_train, x_val, y_val = fold
 
-    if scale_x:
-      x_train, x_val = scale_data(x_train, x_val, columns_to_scale=cols_to_scale)
+            if scale_x:
+                x_train, x_val = scale_data(x_train, x_val)
 
-    model.fit(x_train, y_train)
+            model.fit(x_train, y_train)
 
-    if optimize_thresh:
-      y_pred = model.predict(x_val, prediction_type='Probability')
+            if optimize_thresh:
+                y_pred = model.predict(x_val, prediction_type='Probability')
 
-      optim = optimize_threshold(y_pred, y_val, 3)
-      thresholds = []
+                threshold_optimizer.get_best_thresholds(model_package)
+                
+                thresholds = model_package['thresholds']
 
-      for k, v in optim.items():
-        max_f1_thresh_pair = sorted(v.items(), key=lambda pair: pair[1], reverse=True)[0]
-        max_f1_thresh = max_f1_thresh_pair[0]
+                y_pred = make_preds_with_thresholds(y_pred, thresholds)
+            else:
+                y_pred = model.predict(x_val).flatten()
 
-        avg_threshes[k] = max_f1_thresh
+            f1_per_class = f1_score(y_val, y_pred, average=None)
+            f1s_per_class.append(f1_per_class)
+            avg_f1s.append(np.mean(f1_per_class))
 
-        thresholds.append(max_f1_thresh)
-
-      y_pred = make_preds(y_pred, thresholds)
-    else:
-      y_pred = model.predict(x_val)
-
-    f1_per_class = f1_score(y_val, y_pred, average=None)
-    f1s_per_class.append(f1_per_class)
-    avg_f1s.append(np.mean(f1_per_class))
-
-  print(np.mean(avg_f1s))
-  if optimize_thresh:
-    return [np.mean(threshes) for threshes in avg_threshes]
+        return np.mean(avg_f1s)
