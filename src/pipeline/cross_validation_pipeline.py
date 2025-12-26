71b32a1238d7af96632afe3d3d808c60bd977e4f
bongjoonno <zachary.feryate@gmail.com>
2025-12-26 09:45:11 -0800

    simplified tune hyperparameters function

--- src/pipeline/cross_validation_pipeline.py
+++ src/pipeline/cross_validation_pipeline.py
@@ -3,45 +3,21 @@ from src.data_prep import clean_data, train_df, split_data_k_folds, scale_data
 from src.prediction_creation import make_preds_with_thresholds
 from src.optimizers import HyperParamOptimizer
 
-threshold_optimizer = HyperParamOptimizer()
+hyperparam_optimizer = HyperParamOptimizer()
 
 class CrossValPipeline:
     n_folds = 5
     
     @staticmethod
     def tune_hyperparameters(model_package: dict):
-        model = model_package['model']
-        one_hot_encode_categoricals = model_package['one_hot_encode_categoricals']
-        scale_x = model_package['scale_x']
-        
         optimize_epochs = model_package['optimize_epochs']
         optimize_thresh = model_package['optimize_thresh']
                 
-        df = clean_data(train_df, one_hot_encode_categoricals)
-            
-        x = df.drop(columns='Target')
-        y = df['Target']
-            
-        folds = split_data_k_folds(x, y, CrossValPipeline.n_folds)
-        
-        best_iterations = []
-        
-        for fold in folds:
-            x_train, y_train, x_val, y_val = fold
-
-            if scale_x:
-                x_train, x_val = scale_data(x_train, x_val)
-
-            model.fit(x_train, y_train,
-                      eval_set=(x_val, y_val))
-
-            best_iterations.append(model.get_best_iteration())
-        
         if optimize_epochs:
-            model_package['best_epochs'] = round(np.mean(best_iterations))
+            hyperparam_optimizer.optimize_epochs(model_package)
         
         if optimize_thresh:
-            threshold_optimizer.get_best_thresholds(model_package)
+            hyperparam_optimizer.get_best_thresholds(model_package)
     
     def something_else(model_package: dict):
         model = model_package['model']
@@ -72,7 +48,7 @@ class CrossValPipeline:
             if optimize_thresh:
                 y_pred = model.predict(x_val, prediction_type='Probability')
                 
-                threshold_optimizer.get_best_thresholds(model_package)
+                hyperparam_optimizer.get_best_thresholds(model_package)
                 thresholds = model_package['thresholds']
                 
                 y_pred = make_preds_with_thresholds(y_pred, thresholds)
