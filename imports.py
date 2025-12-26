216fe6c66ae6843b23a73bfb6f32b46620aed7a4
bongjoonno <zachary.feryate@gmail.com>
2025-12-26 08:56:27 -0800

    set numpy random seed, testing...

--- imports.py
+++ imports.py
@@ -1,5 +1,7 @@
 import pandas  as pd
 import numpy as np
+np.random.seed(42)
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import f1_score
 from catboost import CatBoostClassifier
